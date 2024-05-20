8e767b36e006df2df26ce34817617e973a535595
Shenhaoyi <shenhaoyi04@163.com>
2024-05-20 22:04:08 +0800

    finish person module

--- src/modules/person/person.controller.ts
+++ src/modules/person/person.controller.ts
@@ -1,7 +1,47 @@
-import { Controller } from '@nestjs/common';
-import { PersonService } from './person.service';
+import {
+  Body,
+  Controller,
+  Get,
+  Header,
+  Param,
+  Post,
+  Query,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { CreatePersonDto } from './dto/person.dto';
+import { AnyFilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('person')
 export class PersonController {
-  constructor(private readonly personService: PersonService) {}
+  @Get('find')
+  query(@Query() query: { name: string; age: number }) {
+    return `received: name=${query.name},age=${query.age}`;
+  }
+
+  @Get(':id')
+  @Header('Content-Type', 'text/plain')
+  urlParam(@Param('id') id: string) {
+    return `received: id=${id}`;
+  }
+
+  @Post()
+  body(@Body() person: CreatePersonDto) {
+    return `received: ${JSON.stringify(person)}`;
+  }
+
+  @Post('upload')
+  @UseInterceptors(
+    // 任意数量的文件上传
+    AnyFilesInterceptor({
+      dest: 'public/uploads/',
+    }),
+  )
+  uploadFile(
+    @Body() person: CreatePersonDto,
+    @UploadedFile() files: Array<Express.Multer.File>,
+  ) {
+    console.log(person);
+    console.log(files); // TODO: 为啥这里读不出
+  }
 }
--- src/modules/person/person.module.ts
+++ src/modules/person/person.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { PersonService } from './person.service';
 import { PersonController } from './person.controller';
 
 @Module({
   controllers: [PersonController],
-  providers: [PersonService],
 })
 export class PersonModule {}
