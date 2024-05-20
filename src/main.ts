dfb82759c48652314be1a0cf5a7f0183d9c4f605
Shenhaoyi <shenhaoyi04@163.com>
2024-05-20 20:42:23 +0800

    static assets service

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as path from 'node:path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(path.join(process.cwd(), 'public'), {
+    prefix: '/public',
+  });
   await app.listen(3000);
   console.log('Server is running at http://localhost:3000/');
 }
